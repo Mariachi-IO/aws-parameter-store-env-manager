f31f34341e8fc1d1db3f6fceab8781704c6d7954
kenMarquez <darius_dam@ciencias.unam.mx>
2021-08-03 23:00:18 -0500

    Add automatically option and extra validation for environment

--- src/configure.ts
+++ src/configure.ts
@@ -16,21 +16,21 @@ const rl = readline.createInterface({
 function question(message: string) {
   console.clear();
   return new Promise((resolve) =>
-    rl.question(message, (option: string) => {
-      console.clear();
-      resolve(option);
-    }),
+  rl.question(message, (option: string) => {
+    console.clear();
+    resolve(option);
+  }),
   );
 }
 
 export async function configure(params: string[]) {
   try {
     const envNames = config.envs
-      .map((configEnv: ConfigEnv, i: number) => `${i + 1}. ${configEnv.name}.\n`, '')
-      .join('');
+    .map((configEnv: ConfigEnv, i: number) => `${i + 1}. ${configEnv.name}.\n`, '')
+    .join('');
     let option: number;
     if (params[0]) {
-      option = paramAsNumber(params[0]) + 1;
+      option = paramAsNumber(params[0]);
     } else {
       option = (await question(`Which env do you want to configure?\n${envNames}`)) as number;
       if (option < 1 || option > config.envs.length) {
@@ -38,28 +38,32 @@ export async function configure(params: string[]) {
       }
     }
     const env = config.envs[option - 1];
+    if (!env) {
+      console.log("No environment found")
+      return;
+    }
     console.log(`Environment \"${env.name}\" selected`);
 
     const parametersResponses = await Promise.all(
-      env.paths.map(async (path) => {
-        const response = (await fetchParametersByRoute(path)) as string;
-        const parameterResponse = JSON.parse(response) as Parameters;
-        return [...parameterResponse.Parameters];
-      }),
+    env.paths.map(async (path) => {
+      const response = (await fetchParametersByRoute(path)) as string;
+      const parameterResponse = JSON.parse(response) as Parameters;
+      return [...parameterResponse.Parameters];
+    }),
     );
     let content = '';
     parametersResponses.forEach((parameters) =>
-      parameters.forEach((parameter) => {
-        const path = parameter.Name;
-        const index = path.lastIndexOf('/');
-        const name = path.substring(index + 1, path.length);
-        let date = '';
-        if (config.enableUpdateDate) {
-          date = `  # updated - ${getDate(new Date(parameter.LastModifiedDate))}`;
-        }
-        console.log(`${name}=${parameter.Value}${date}`);
-        content += `${name}=${parameter.Value}${date}\n`;
-      }),
+    parameters.forEach((parameter) => {
+      const path = parameter.Name;
+      const index = path.lastIndexOf('/');
+      const name = path.substring(index + 1, path.length);
+      let date = '';
+      if (config.enableUpdateDate) {
+        date = `  # updated - ${getDate(new Date(parameter.LastModifiedDate))}`;
+      }
+      console.log(`${name}=${parameter.Value}${date}`);
+      content += `${name}=${parameter.Value}${date}\n`;
+    }),
     );
     const dateContent = `# [env-manager] automatically updated on ${getDate(new Date())}\n`;
     fs.writeFileSync(config.filePath, dateContent.concat(content));
