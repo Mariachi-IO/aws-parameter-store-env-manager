63319dde4839f1cb553573d7c805b516afaf5e8e
kenMarquez <darius_dam@ciencias.unam.mx>
2021-08-03 22:09:40 -0500

    Add validation for a config file

--- src/cli.ts
+++ src/cli.ts
@@ -2,14 +2,23 @@
 
 // Grab provided args
 import { configure } from './configure';
+import { fileExists } from "./utils";
+const Path = require('path')
 
 const [, , ...args] = process.argv;
 
-console.log('Hello world ' + args);
-
 switch (args[0]) {
   case 'configure':
-    configure(args.slice(1)).then(() => process.exit(0));
+    const path = Path.join?.(__dirname, "../envConfig.js")
+    fileExists(path).then((exists)=>{
+      if (exists){
+        configure(args.slice(1)).then(() => process.exit(0));
+      }else {
+        console.log('Cannot find the file: envConfig.js \n')
+        console.log('For more references, see the documentation.: ','https://github.com/Mariachi-IO/aws-parameter-store-env-manager')
+        process.exit(0)
+      }
+    })
     break;
   default:
     process.exit(0);
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,18 @@
+const { promises: Fs } = require('fs')
+
 export function getDate(date: Date) {
   return date.toISOString().replace(/T/, ' ').replace(/\..+/, '');
 }
 
 export function paramAsNumber(option: string): number {
   return +option;
+}
+
+export async function fileExists(path: string | undefined) {
+  try {
+    await Fs.access(path)
+    return true
+  } catch {
+    return false
+  }
 }
